e7068ab8d2d903510f64a729ed4c1d77e1a05cb3
Geff <gyndok@yahoo.com>
2023-02-02 18:47:09 -0600

    Initial Commit

---
+++ SwiftUITableDraw/SwiftUITableDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableDrawApp.swift
+//  SwiftUITableDraw
+//
+//  Created by Geffrey Klein on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
