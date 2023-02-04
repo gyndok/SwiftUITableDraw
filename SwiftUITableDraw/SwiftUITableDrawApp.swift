e74d73e1cae5570a6b48256de48a1dc20e9190bf
Geffrey Klein <gyndok@users.noreply.github.com>
2023-02-04 17:49:02 -0600

    added firebase

--- SwiftUITableDraw/SwiftUITableDrawApp.swift
+++ SwiftUITableDraw/SwiftUITableDrawApp.swift
@@ -5,10 +5,21 @@
 //  Created by Geffrey Klein on 2/2/23.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct SwiftUITableDrawApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             NavigationStack{
