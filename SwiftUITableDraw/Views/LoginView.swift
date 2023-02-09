ea7a36df9c2ede43f6de100d87ef36162c4feb4f
Geffrey Klein <gyndok@users.noreply.github.com>
2023-02-09 08:10:02 -0600

    login screen changes

--- SwiftUITableDraw/Views/LoginView.swift
+++ SwiftUITableDraw/Views/LoginView.swift
@@ -22,148 +22,170 @@ struct LoginView: View {
     
     var body: some View {
         
-        ZStack (alignment: .top) {
-            Color.brown
-                .ignoresSafeArea(.all)
-            VStack (alignment: .center, spacing: 15){
-                Text("My Table Draw")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(width: 300, height:30)
-                    .padding()
-                    .foregroundColor(.brown)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                
-                
-                Image("TableDraw")
-                    .resizable()
-                    .frame (width: 190, height: 190)
-                
-                
-                HStack {
-                    if !showLogin {
-                        Button(action: {
-                            self.showLogin.toggle()
-                        }) {
-                            Text("Switch to Login")
-                            
-                                .font(.headline)
-                        }.font(.headline)
-                            .frame(height:45)
-                            .frame(width: 220)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                        
-                    } else if showLogin{
-                        Button(action: {
-                            self.showLogin.toggle()
-                        }) {
-                            Text("I Am New Here")
-                                .font(.headline)
-                        }.font(.headline)
-                            .frame(height:45)
-                            .frame(width: 220)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(10)
-                        
-                    }
-                }
-                .padding()
-                //Spacer()
-                
-                
-                if showLogin {
-                    VStack {
-                        TextField("Login", text: $login)
-                            .padding(10)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Password", text: $password)
-                            .padding(10)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                       
-                        Button(action: {
-                            Auth.auth().signIn(withEmail: self.login, password: self.password) { (result, error) in
-                                if error != nil {
-                                    print("Login failed: \(error!.localizedDescription)")
-                                    return
-                                }
-                                print("Login successful")
-                            }
-                        }) {
-                            Text("Sign In")
-                        }
-                        .font(.headline)
-                        .frame(height:45)
-                        .frame(width: 100)
+        NavigationStack {
+            ZStack (alignment: .top) {
+                Color.brown
+                    .ignoresSafeArea(.all)
+                VStack (alignment: .center, spacing: 15){
+                    Text("My Table Draw")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .frame(width: 300, height:30)
+                        .padding()
                         .foregroundColor(.brown)
                         .background(Color.white)
                         .cornerRadius(10)
-                    }
                     
-                } else {
-                    VStack {
-                        TextField("First Name", text: $firstName)
-                            .padding(10)
-                            .autocorrectionDisabled(true)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        TextField("Last Name", text: $lastName)
-                            .padding(10)
-                            .autocorrectionDisabled(true)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        TextField("Email Address", text: $email)
-                            .padding(10)
-                            .autocorrectionDisabled(true)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Password", text: $password)
-                            .padding(10)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Confirm Password", text: $passwordConfirm)
-                            .padding(10)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        
-                        
-                        
-                        Button(action: {
-                            Auth.auth().createUser(withEmail: self.email, password: self.password) { (result, error) in
-                                if error != nil {
-                                    print("Registration failed: \(error!.localizedDescription)")
-                                    return
-                                }
-                                print("Registration successful")
-                            }
-                        }) {
-                            Text("Register")
+                    
+                    Image("TableDraw")
+                        .resizable()
+                        .frame (width: 190, height: 190)
+                    
+                    
+                    HStack {
+                        if !showLogin {
+                            Button(action: {
+                                self.showLogin.toggle()
+                                password = ""
+                                login = ""
+                                firstName = ""
+                                lastName = ""
+                                email = ""
+                                passwordConfirm = ""
+                            }) {
+                                Text("Switch to Login")
+                                
+                                    .font(.headline)
+                            }.font(.headline)
+                                .frame(height:45)
+                                .frame(width: 220)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            
+                        } else if showLogin{
+                            Button(action: {
+                                self.showLogin.toggle()
+                                password = ""
+                                login = ""
+                                firstName = ""
+                                lastName = ""
+                                email = ""
+                            }) {
+                                Text("I Am New Here")
+                                    .font(.headline)
+                                   
+                            }.font(.headline)
+                                .frame(height:45)
+                                .frame(width: 220)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                               
+                            
                         }
-                        .font(.headline)
-                        .frame(height:45)
-                        .frame(width: 100)
-                        .foregroundColor(.brown)
-                        .background(Color.white)
-                        .cornerRadius(10)
+                            
                         
+                        
+                    }
+                    .padding()
+                  
+                    
+                    
+                    if showLogin {
+                        VStack {
+                            TextField("Login", text: $login)
+                                .padding(10)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Password", text: $password)
+                                .padding(10)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                           
+                            Button(action: {
+                                Auth.auth().signIn(withEmail: self.login, password: self.password) { (result, error) in
+                                    if error != nil {
+                                        print("Login failed: \(error!.localizedDescription)")
+                                        return
+                                    }
+                                    print("Login successful")
+                                }
+                            }) {
+                                Text("Sign In")
+                            }
+                            .font(.headline)
+                            .frame(height:45)
+                            .frame(width: 100)
+                            .foregroundColor(.brown)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        }
+                        
+                    } else {
+                        VStack {
+                            TextField("First Name", text: $firstName)
+                                .padding(10)
+                                .autocorrectionDisabled(true)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            TextField("Last Name", text: $lastName)
+                                .padding(10)
+                                .autocorrectionDisabled(true)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            TextField("Email Address", text: $email)
+                                .padding(10)
+                                .autocorrectionDisabled(true)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Password", text: $password)
+                                .padding(10)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Confirm Password", text: $passwordConfirm)
+                                .padding(10)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            
+                            
+                            
+                            Button(action: {
+                                
+                                Auth.auth().createUser(withEmail: self.email, password: self.password) { (result, error) in
+                                    if error != nil {
+                                        print("Registration failed: \(error!.localizedDescription)")
+                                        return
+                                    }
+                                   
+                                    print("Registration successful")
+                                }
+                            }) {
+                                Text("Register")
+                            }
+                            .font(.headline)
+                            .frame(height:45)
+                            .frame(width: 100)
+                            .foregroundColor(.brown)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            
+                        }
                     }
                 }
             }
         }
     }
 }
+
+
     
 
     
--- SwiftUITableDraw/Views/PlayerCell.swift
+++ SwiftUITableDraw/Views/PlayerCell.swift
@@ -18,50 +18,48 @@ struct PlayerCell: View {
     
     var body: some View {
         
-            HStack(spacing: 30){
-                Image(systemName: player.symbol)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundColor(.brown)
-                    .frame(minWidth: 25, maxWidth: 25, alignment: .center)
-                    .padding()
-                    .shadow(color: Color.brown.opacity(0.6),
-                            radius: 10,
-                            x: 0.0, y:12)
-                  
-                
-                    VStack{
-                    
-                        Text(player.playerName)
-                            .foregroundColor(.primary)
-                            .font(.subheadline)
-                            .fontWeight(.bold)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .underline()
-                        
-                        Text(player.hometown)
-                            .minimumScaleFactor(0.7)
-                               .lineLimit(1)
-                            .foregroundColor(.primary)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                        HStack{
-                            Text("Chips: \(player.chipCount) ")
-                            Text("\(player.bigBlindsString)")
-                        }
-                        .foregroundColor(.secondary)
-                        .font(.caption)
-                        .fontWeight(.semibold)
-                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    }
-              
-            }
-        
+        HStack(spacing: 30){
+            Image(systemName: player.symbol)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.brown)
+                .frame(minWidth: 25, maxWidth: 25, alignment: .center)
+                .padding()
+                .shadow(color: Color.brown.opacity(0.6),
+                        radius: 10,
+                        x: 0.0, y:12)
             
-
+            
+            VStack{
+                
+                Text(player.playerName)
+                    .foregroundColor(.primary)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                    .underline()
+                
+                Text(player.hometown)
+                    .minimumScaleFactor(0.7)
+                    .lineLimit(1)
+                    .foregroundColor(.primary)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                HStack{
+                    Text("Chips: \(player.chipCount) ")
+                    Text("\(player.bigBlindsString)")
+                }
+                .foregroundColor(.secondary)
+                .font(.caption)
+                .fontWeight(.semibold)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            }
+            
         }
+        
     }
+}
 
 
 struct PlayerCell_Previews: PreviewProvider {
