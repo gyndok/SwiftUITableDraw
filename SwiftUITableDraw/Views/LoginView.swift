08b64926c70c6d81247ee48de474d5cc8733a2b0
Geffrey Klein <gyndok@users.noreply.github.com>
2023-02-08 19:00:58 -0600

    working on navigationView

--- SwiftUITableDraw/Views/LoginView.swift
+++ SwiftUITableDraw/Views/LoginView.swift
@@ -21,26 +21,26 @@ struct LoginView: View {
     @State private var passwordConfirm = ""
     
     var body: some View {
-        
-        ZStack (alignment: .bottom) {
-            Color.brown
-                .ignoresSafeArea(.all)
-            VStack (alignment: .center, spacing: 15){
-                Text("My Table Draw")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(width: 300)
-                    .padding()
-                    .foregroundColor(.brown)
-                    .background(Color.white)
-                    .cornerRadius(10)
-                
-    
-                Image("TableDraw")
-                    .resizable()
-                    .frame (width: 215, height: 200)
-               
-                
+        NavigationView{
+            ZStack (alignment: .top) {
+                Color.brown
+                    .ignoresSafeArea(.all)
+                VStack (alignment: .center, spacing: 15){
+                    Text("My Table Draw")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .frame(width: 300, height:30)
+                        .padding()
+                        .foregroundColor(.brown)
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    
+                    
+                    Image("TableDraw")
+                        .resizable()
+                        .frame (width: 190, height: 190)
+                    
+                    
                     HStack {
                         if !showLogin {
                             Button(action: {
@@ -55,7 +55,7 @@ struct LoginView: View {
                                 .foregroundColor(.brown)
                                 .background(Color.white)
                                 .cornerRadius(10)
-                                
+                            
                         } else if showLogin{
                             Button(action: {
                                 self.showLogin.toggle()
@@ -68,116 +68,95 @@ struct LoginView: View {
                                 .foregroundColor(.brown)
                                 .background(Color.white)
                                 .cornerRadius(10)
-                                
+                            
                         }
                     }
                     .padding()
-                Spacer()
-                
-                
-                if showLogin {
-                    VStack {
-                        TextField("Login", text: $login)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Password", text: $password)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        NavigationLink(destination: TourneyListView()){
-                            Button(action: {
-                                Auth.auth().signIn(withEmail: self.login, password: self.password) { (result, error) in
-                                    if error != nil {
-                                        print("Login failed: \(error!.localizedDescription)")
-                                        return
+                    //Spacer()
+                    
+                    
+                    if showLogin {
+                        VStack {
+                            TextField("Login", text: $login)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Password", text: $password)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            NavigationLink(destination: TourneyListView()){
+                                Button(action: {
+                                    Auth.auth().signIn(withEmail: self.login, password: self.password) { (result, error) in
+                                        if error != nil {
+                                            print("Login failed: \(error!.localizedDescription)")
+                                            return
+                                        }
+                                        print("Login successful")
                                     }
-                                    print("Login successful")
+                                }) {
+                                    Text("Sign In")
                                 }
-                            }) {
-                                Text("Sign In")
+                                .font(.headline)
+                                .frame(height:45)
+                                .frame(width: 100)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(10)
                             }
-                            .font(.headline)
-                            .frame(height:45)
-                            .frame(width: 100)
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(10)
+                        }
+                    } else {
+                        VStack {
+                            TextField("First Name", text: $firstName)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            TextField("Last Name", text: $lastName)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            TextField("Email Address", text: $email)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Password", text: $password)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            SecureField("Confirm Password", text: $passwordConfirm)
+                                .padding()
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(5.0)
+                            
+                            
+                            NavigationLink(destination: TourneyListView()) {
+                                Button(action: {
+                                    Auth.auth().createUser(withEmail: self.email, password: self.password) { (result, error) in
+                                        if error != nil {
+                                            print("Registration failed: \(error!.localizedDescription)")
+                                            return
+                                        }
+                                        print("Registration successful")
+                                    }
+                                }) {
+                                    Text("Register")
+                                }
+                                .font(.headline)
+                                .frame(height:45)
+                                .frame(width: 100)
+                                .foregroundColor(.brown)
+                                .background(Color.white)
+                                .cornerRadius(10)
+                            }
                         }
                     }
-                } else {
-                    VStack {
-                        TextField("First Name", text: $firstName)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        TextField("Last Name", text: $lastName)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        TextField("Email Address", text: $email)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Password", text: $password)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                        SecureField("Confirm Password", text: $passwordConfirm)
-                            .padding()
-                            .foregroundColor(.brown)
-                            .background(Color.white)
-                            .cornerRadius(5.0)
-                       
-                        
-                        NavigationLink(destination: TourneyListView()) {
-                          Button(action: {
-                            Auth.auth().createUser(withEmail: self.email, password: self.password) { (result, error) in
-                              if error != nil {
-                                print("Registration failed: \(error!.localizedDescription)")
-                                return
-                              }
-                              print("Registration successful")
-                            }
-                          }) {
-                            Text("Register")
-                          }
-                          .font(.headline)
-                          .frame(height:45)
-                          .frame(width: 100)
-                          .foregroundColor(.brown)
-                          .background(Color.white)
-                          .cornerRadius(10)
-                        }
-
-
-//                        Button(action: {
-//                            if password == passwordConfirm{
-//                                LoginUserDefaults.set("\(firstName) \(lastName)", forKey: K.searchPlayerNameKey)
-//                                Auth.auth().createUser(withEmail: self.email, password: self.password) { (result, error) in
-//                                if error != nil {
-//                                    print("Registration failed: \(error!.localizedDescription)")
-//                                    return
-//                                }
-//                                print("Registration successful")
-//                                }
-//                        }
-//                        }) {
-//                            Text("Register")
-//                        }
-//                        .font(.headline)
-//                            .frame(height:45)
-//                            .frame(width: 100)
-//                            .foregroundColor(.brown)
-//                            .background(Color.white)
-//                            .cornerRadius(10)
-                   }
                 }
             }
         }
@@ -186,6 +165,8 @@ struct LoginView: View {
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        NavigationView(){
+            LoginView()
+        }
     }
 }
