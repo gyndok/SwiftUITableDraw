c155ecba4a54e4542c16655ccec5f18747165cd1
Harrison Klein <harrisonklein9@gmail.com>
2023-02-10 13:58:35 -0800

    Fix navigation, looks like there is a weird space in the top though, might be a navigation bar thing

--- SwiftUITableDraw/Views/TourneyListView.swift
+++ SwiftUITableDraw/Views/TourneyListView.swift
@@ -26,52 +26,52 @@ struct TourneyListView: View {
     @State var searchPlayer: String = userDefaults.object(forKey: K.searchPlayerNameKey) as? String ?? ""
     
     
-    var body: some View {
-        
-        
-        VStack (spacing: 10) {
-            TextField("Who are you looking for?", text: $searchPlayer)
-                .autocorrectionDisabled(true)
-                .onTapGesture {
-                    print("tapped")
-                }
-                .padding(.horizontal)
-                .frame(height: 55)
-                .background(Color(.systemGray6))
-                .cornerRadius(10)
-            Button (action:{
-                
-                userDefaults.set(searchPlayer, forKey: K.searchPlayerNameKey)
-                tourneys.removeAll()
-                fetchTourneys()
-               
-                
-            }, label: {
-                Text("Find The Tables")
-                    .font(.headline)
-                    .frame(height:45)
-                    .frame(width: 360)
-                    .foregroundColor(.brown)
-                    .background(Color.white)
-                    .cornerRadius(10)
-            })
-            .padding()
-            .background(
-                Color.brown)
-            .shadow(color: Color.black.opacity(0.3),
-                    radius: 10,
-                    x: 0.0, y:12)
-        }
-        
+  var body: some View {
+    
+    NavigationView {
+      VStack (spacing: 10) {
+        TextField("Who are you looking for?", text: $searchPlayer)
+          .autocorrectionDisabled(true)
+          .onTapGesture {
+            print("tapped")
+          }
+          .padding(.horizontal)
+          .frame(height: 55)
+          .background(Color(.systemGray6))
+          .cornerRadius(10)
+        Button (action:{
+          
+          userDefaults.set(searchPlayer, forKey: K.searchPlayerNameKey)
+          tourneys.removeAll()
+          fetchTourneys()
+          
+          
+        }, label: {
+          Text("Find The Tables")
+            .font(.headline)
+            .frame(height:45)
+            .frame(width: 360)
+            .foregroundColor(.brown)
+            .background(Color.white)
+            .cornerRadius(10)
+        })
+        .padding()
+        .background(
+          Color.brown)
+        .shadow(color: Color.black.opacity(0.3),
+                radius: 10,
+                x: 0.0, y:12)
         List {
-            ForEach(tourneys) { tourney in
-                NavigationLink(destination: PlayerListView()) {
-                    TourneyCell(tourney: tourney)
-                }
+          ForEach(tourneys) { tourney in
+            NavigationLink(destination: PlayerListView()) {
+              TourneyCell(tourney: tourney)
             }
+          }
         }
         .listStyle(PlainListStyle())
+      }
     }
+  }
     
     
     
